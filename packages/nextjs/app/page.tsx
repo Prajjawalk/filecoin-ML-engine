a1f25b0d9be2308cf81a7924464c33a3d217c0c0
Prajjawalk <prajjawalkhandelwal@gmail.com>
2024-05-24 15:49:06 +0530

    Add: schema files

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,67 +1,93 @@
 "use client";
 
-import Link from "next/link";
+import { useEffect, useState } from "react";
+import { CreateSchema } from "./_components/schema/CreateSchema";
+import { SchemaDetails } from "./_components/schema/SchemaDetails";
 import type { NextPage } from "next";
-import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { Address } from "~~/components/scaffold-eth";
+import { Abi, WalletClient, createPublicClient, createWalletClient, getContract } from "viem";
+import { filecoinCalibration } from "viem/chains";
+import { custom, http } from "wagmi";
+import deployedContracts from "~~/contracts/deployedContracts";
 
 const Home: NextPage = () => {
-  const { address: connectedAddress } = useAccount();
+  const [schemas, setSchemas] = useState<any>();
+  const [odl, setOdl] = useState<any>();
+
+  useEffect(() => {
+    let walletClient: WalletClient;
+    if (window.ethereum) {
+      walletClient = createWalletClient({
+        chain: filecoinCalibration,
+        transport: custom(window.ethereum as any),
+      });
+    }
+
+    const publicClient = createPublicClient({
+      chain: filecoinCalibration,
+      transport: http(),
+    });
+
+    const getSchemas = async () => {
+      try {
+        const userAnalyticsContractData = getContract({
+          abi: deployedContracts[31337].DataLayer.abi as Abi,
+          address: deployedContracts[31337].DataLayer.address,
+          client: { public: publicClient, wallet: walletClient },
+        });
+        setOdl(userAnalyticsContractData);
+
+        const schemas_ = await userAnalyticsContractData.read.getAllSchemas();
+
+        console.log(schemas_);
+        setSchemas(schemas_);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    getSchemas();
+  }, []);
 
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
           <h1 className="text-center">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-4xl font-bold heading">Open Data Library (ODL)</span>
+            <span className="block text-2xl mb-2 des">Dashboard</span>
+            <div className="padding"></div>
           </h1>
-          <div className="flex justify-center items-center space-x-2">
-            <p className="my-2 font-medium">Connected Address:</p>
-            <Address address={connectedAddress} />
+        </div>
+        <div className="grid grid-cols-2 gap-10 w-full max-w-7xl">
+          <div className="z-10">
+            <div className="adafelbg bg-base-100 rounded-3xl  border-base-300 flex flex-col mt-10 relative">
+              <div className="h-[5rem] w-[5.5rem] bg-base-300 absolute self-start rounded-[22px] -top-[38px] -left-[1px] -z-10 py-[0.65rem] shadow-lg">
+                <div className="flex items-center justify-center space-x-2">
+                  <p className="my-0 text-sm">Schemas</p>
+                </div>
+              </div>
+              <div className="p-5 divide-y divide-base-300 h-screen overflow-scroll">
+                {/* Use only if required overflow-scroll */}
+                <SchemaDetails schemaList={schemas} odl={odl} />
+              </div>
+            </div>
           </div>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
+          <div className="z-10">
+            <div className="adafelbg bg-base-100 rounded-3xl  border-base-300 flex flex-col mt-10 relative">
+              <div className="h-[5rem] w-[5.5rem] bg-base-300 absolute self-start rounded-[22px] -top-[38px] -left-[1px] -z-10 py-[0.65rem] shadow-lg">
+                <div className="flex items-center justify-center space-x-2">
+                  <p className="my-0 text-sm">Create</p>
+                </div>
+              </div>
+              <div className="p-5 divide-y divide-base-300">
+                <CreateSchema odl={odl} />
+              </div>
+            </div>
+          </div>
         </div>
 
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-          </div>
+        <div className="flex-grow w-full mt-16 px-8 py-12">
+          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row"></div>
         </div>
       </div>
     </>
